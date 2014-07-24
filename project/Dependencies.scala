6b07b80642a824d710fb1532fe8e64f033721786
Robert Ferris <robertf@avi.com>
2014-07-24 17:44:19 -0400

    Update to Dependency Check 1.2.3.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,5 +61,5 @@ object Dependencies {
 	}
 
 	// dependency-check
-	lazy val dependencyCheckCore = "org.owasp" % "dependency-check-core" % "1.2.1"
+	lazy val dependencyCheckCore = "org.owasp" % "dependency-check-core" % "1.2.3"
 }
