93e473af252a2cdf204dda5bf6c71473fe7017d9
Hassan Radwan <hassanr@avi.com>
2015-07-08 17:09:17 -0400

    Updating the version/date for v1.1.3

--- project/Build.scala
+++ project/Build.scala
@@ -45,8 +45,8 @@ object BuildDef extends Build with VersionSystem {
 
 	val baseProjectSettings = net.virtualvoid.sbt.graph.Plugin.graphSettings ++ baseCompilerSettings ++ Seq(
 		organization := "com.avi",
-		version := "1.1.2",
-		releaseDate := "2/26/2015"
+		version := "1.1.3",
+		releaseDate := "7/9/2015"
 	)
 
 	val webappProjectSettings = WebPlugin.webSettings ++ Seq (
