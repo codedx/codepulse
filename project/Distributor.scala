3134b814d24212494924c13d3e99ea20f3dd7220
Dylan Halperin <dylan@dylemma.io>
2015-09-22 15:19:38 -0400

    update jetty mirror link

--- project/Distributor.scala
+++ project/Distributor.scala
@@ -120,7 +120,7 @@ object Distributor extends BuildExtra {
 				.to { _ / "distrib-dependencies" / "osx" / "nwjs" }
 		}
 
-		val jetty = Dependency("jetty", "9.3.0-v20150612", "http://mirrors.xmission.com/eclipse/jetty/stable-9/dist/jetty-distribution-9.3.0.v20150612.zip")
+		val jetty = Dependency("jetty", "9.3.0-v20150612", "http://mirrors.xmission.com/eclipse/jetty/stable-9/dist/jetty-distribution-9.3.3.v20150827.zip")
 			.extractAsZip { raw"^\Qjetty-distribution-9.3.0.v20150612\E/".r.replaceFirstIn(_, "") }
 			.to { _ / "distrib-dependencies" / "common" / "jetty" }
 
