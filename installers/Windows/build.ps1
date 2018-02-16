6bbd5883e62b46d178f0d244782c79c1aa02f109
Steve Salas <ssalas@codedx.com>
2018-02-16 13:12:57 -0500

    Initial version of Windows installer
    Adds PowerShell script to generate .wxs installer project files
    Build script now creates Windows installer package
    Sets working directory when launching .NET Symbol Service
    Run chmodSync on java based on accessSync result

--- installers/Windows/build.ps1
+++ installers/Windows/build.ps1
@@ -2,6 +2,10 @@
 $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 
+Push-Location $PSScriptRoot
+
+. .\text.ps1
+
 # NOTE: build will not work with C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 $msbuildPath = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe'
 
@@ -40,12 +44,14 @@ New-Item -Path $filesFolderPath -ItemType Directory
 $filesFolderWin32Path = join-path $filesFolderPath 'Win32'
 $filesFolderWin32CodePulsePath = join-path $filesFolderWin32Path 'codepulse'
 $filesFolderWin32DotNetSymbolServicePath = join-path $filesFolderWin32Path 'dotnet-symbol-service'
-$filesFolderWin32DotNetTracerPath = join-path $filesFolderWin32Path 'dotnet-tracer'
+$filesFolderWin32DotNetTracerPath = join-path $filesFolderWin32Path 'tracers\dotnet'
+$filesFolderWin32JavaTracerPath = join-path $filesFolderWin32Path 'tracers\java'
 
 $filesFolderWin64Path = join-path $filesFolderPath 'Win64'
 $filesFolderWin64CodePulsePath = join-path $filesFolderWin64Path 'codepulse'
 $filesFolderWin64DotNetSymbolServicePath = join-path $filesFolderWin64Path 'dotnet-symbol-service'
-$filesFolderWin64DotNetTracerPath = join-path $filesFolderWin64Path 'dotnet-tracer'
+$filesFolderWin64DotNetTracerPath = join-path $filesFolderWin64Path 'tracers\dotnet'
+$filesFolderWin64JavaTracerPath = join-path $filesFolderWin64Path 'tracers\java'
 
 write-verbose 'Creating Files folder structure...'
 #
@@ -63,10 +69,12 @@ New-Item -Path $filesFolderWin32Path -ItemType Directory
 New-Item -Path $filesFolderWin32CodePulsePath -ItemType Directory
 New-Item -Path $filesFolderWin32DotNetSymbolServicePath -ItemType Directory
 New-Item -Path $filesFolderWin32DotNetTracerPath -ItemType Directory
+New-Item -Path $filesFolderWin32JavaTracerPath -ItemType Directory
 New-Item -Path $filesFolderWin64Path -ItemType Directory
 New-Item -Path $filesFolderWin64CodePulsePath -ItemType Directory
 New-Item -Path $filesFolderWin64DotNetSymbolServicePath -ItemType Directory
 New-Item -Path $filesFolderWin64DotNetTracerPath -ItemType Directory
+New-Item -Path $filesFolderWin64JavaTracerPath -ItemType Directory
 
 $codePulsePath = join-path $PSScriptRoot '..\..'
 $dotNetSymbolServicePath = join-path $codePulsePath 'dotnet-symbol-service'
@@ -74,6 +82,12 @@ $dotNetTracerPath = join-path $codePulsePath 'dotnet-tracer'
 $dotNetTracerMainPath = join-path $dotNetTracerPath 'main'
 
 Push-Location $codePulsePath
+
+write-verbose "Editing '$applicationConfPath' for packaging..."
+$applicationConfPath = join-path (get-location) 'codepulse\src\main\resources\application.conf'
+$applicationConf = gc $applicationConfPath
+$applicationNewConf = $applicationConf | % { $_ -replace 'dotnet-symbol-service/publish/','../dotnet-symbol-service/' }
+Set-TextContent $applicationConfPath $applicationNewConf
 
 write-verbose 'Packaging Code Pulse (Win32)...'
 Invoke-Sbt packageEmbeddedWin32
@@ -89,16 +103,25 @@ write-verbose 'Unzipping Code Pulse package (Win64)...'
 $win64CodePulsePackagePath = join-path (get-location) ".\codepulse\target\scala-2.10\CodePulse-$($codePulseVersion)-SNAPSHOT-win64.zip"
 [io.compression.zipfile]::ExtractToDirectory($win64CodePulsePackagePath, $filesFolderWin64CodePulsePath)
 
+write-verbose "Restoring original '$applicationConfPath' contents..."
+Set-TextContent $applicationConfPath $applicationConf
+
+write-verbose 'Moving Java agent (Win32)...'
+move-item (join-path $filesFolderWin32CodePulsePath 'codepulse\agent.jar') $filesFolderWin32JavaTracerPath
+
+write-verbose 'Moving Java agent (Win64)...'
+move-item (join-path $filesFolderWin64CodePulsePath 'codepulse\agent.jar') $filesFolderWin64JavaTracerPath
+
 Pop-Location; Push-Location $dotNetSymbolServicePath
 
 write-verbose 'Publishing .NET Symbol Service (Win32)...'
-dotnet publish -c $buildConfiguration -r win10-x86 -o $filesFolderWin32DotNetSymbolServicePath
+dotnet publish -c $buildConfiguration -r win7-x86 -o $filesFolderWin32DotNetSymbolServicePath
 if ($lastexitcode -ne 0) {
     exit $lastexitcode
 }
 
 write-verbose 'Publishing .NET Symbol Service (Win64)...'
-dotnet publish -c $buildConfiguration -r win10-x64 -o $filesFolderWin64DotNetSymbolServicePath
+dotnet publish -c $buildConfiguration -r win7-x64 -o $filesFolderWin64DotNetSymbolServicePath
 if ($lastexitcode -ne 0) {
     exit $lastexitcode
 }
@@ -134,34 +157,37 @@ if ($lastexitcode -ne 0) {
 
 $installerConfigurationName = "$buildConfiguration-Installer"
 
-write-verbose "Building .NET Tracer ($installerConfigurationName | x86)..."
-& $msbuildPath /p:Configuration=$installerConfigurationName /p:Platform=x86 OpenCover.sln
-if ($lastexitcode -ne 0) {
-    exit $lastexitcode
-}
-
-write-verbose "Building .NET Tracer installer ($buildConfiguration | x86)..."
-& $msbuildPath /p:Configuration=$buildConfiguration /p:Platform=x86 CodePulse.Installer
-if ($lastexitcode -ne 0) {
-    exit $lastexitcode
-}
-
-$msiTracerWin32Path = join-path (get-location) ".\CodePulse.Installer\bin\$buildConfiguration\CodePulse.DotNet.Tracer.msi"
-copy-item $msiTracerWin32Path $filesFolderWin32DotNetTracerPath
-
 write-verbose "Building .NET Tracer ($installerConfigurationName | x64)..."
 & $msbuildPath /p:Configuration=$installerConfigurationName /p:Platform=x64 OpenCover.sln
 if ($lastexitcode -ne 0) {
     exit $lastexitcode
 }
 
-write-verbose "Building .NET Tracer installer ($buildConfiguration | x64)..."
-& $msbuildPath /p:Configuration=$buildConfiguration /p:Platform=x64 CodePulse.Installer.x64
+write-verbose "Building .NET Tracer ($installerConfigurationName | x86)..."
+& $msbuildPath /p:Configuration=$installerConfigurationName /p:Platform=x86 OpenCover.sln
 if ($lastexitcode -ne 0) {
     exit $lastexitcode
 }
 
-$msiTracerWin64Path = join-path (get-location) ".\CodePulse.Installer.x64\bin\x64\$buildConfiguration\CodePulse.DotNet.Tracer.x64.msi"
-copy-item $msiTracerWin64Path $filesFolderWin64DotNetTracerPath
+$msiTracerBundlePath = join-path (get-location) ".\CodePulse.Bundle\bin\$buildConfiguration\CodePulse.DotNet.Tracer.Bundle.exe"
+copy-item $msiTracerBundlePath $filesFolderWin32DotNetTracerPath
+copy-item $msiTracerBundlePath $filesFolderWin64DotNetTracerPath
 
-Pop-Location
+Pop-Location; Push-Location $PSScriptRoot
+
+write-verbose 'Running heat.ps1...'
+.\heat.ps1
+
+write-verbose "Building Code Pulse installer ($buildConfiguration | x64)..."
+& $msbuildPath /p:Configuration=$buildConfiguration /p:Platform=x64 Windows.sln
+if ($lastexitcode -ne 0) {
+    exit $lastexitcode
+}
+
+write-verbose "Building Code Pulse installer ($buildConfiguration | x86) and bundle..."
+& $msbuildPath /p:Configuration=$buildConfiguration /p:Platform=x86 Windows.sln
+if ($lastexitcode -ne 0) {
+    exit $lastexitcode
+}
+
+1..2 | % { Pop-Location }
