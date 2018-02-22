7d71ce783512dc24cccdf91660e59e9aa6110af3
Steve Salas <ssalas@codedx.com>
2018-02-22 16:41:35 -0500

    Include Downloads folder in sbt search

--- installers/Windows/build.ps1
+++ installers/Windows/build.ps1
@@ -45,9 +45,23 @@ write-verbose "Using msbuild.exe at $msbuildPath"
 write-verbose 'Adding type for unzip support...'
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+function Get-SbtPath() {
+    try { 
+	    $sbtPaths = C:\Windows\System32\where.exe sbt 2> $null 
+	    $sbtPaths[0]
+    } 
+    catch {
+        $sbtPath = join-path $($env:USERPROFILE) 'Downloads\sbt\bin\sbt'
+        if (-not (test-path $sbtPath)) {
+            throw 'The sbt path cannot be found.'
+        }
+		$sbtPath
+    }
+}
+
 function Invoke-Sbt([string] $packageName) {
     try {
-        sbt $packageName
+        & (Get-SbtPath) $packageName
     }
     catch {
         if ($_.Exception.Message -ne 'Java HotSpot(TM) 64-Bit Server VM warning: ignoring option MaxPermSize=256m; support was removed in 8.0') {
