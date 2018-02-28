16490db65b7649926f3e2953653ec43d39be962b
Steve Salas <ssalas@codedx.com>
2018-02-28 14:01:26 -0500

    Adds 64-bit Linux package and drops 32-bit support

--- installers/build.ps1
+++ installers/build.ps1
@@ -3,13 +3,14 @@
 #
 # 1. git clone https://github.com/codedx/codepulse -b <branch>
 # 2. change directory to .\installers
-# 3. run: powershell -file .\build.ps1 [-skipInit] [-skipWindows] [-skipMac]
+# 3. run: powershell -file .\build.ps1 [-skipInit] [-skipWindows] [-skipMac] [-skipLinux]
 # 4. locate generated Code Pulse packages in this directory
 #
 param (
 	[switch] $skipInit,
 	[switch] $skipWindows,
-	[switch] $skipMac
+	[switch] $skipMac,
+	[switch] $skipLinux
 )
 
 Set-PSDebug -Strict
@@ -23,6 +24,9 @@ if (-not $skipInit) {
 	.\Scripts\init.ps1
 }
 
+Write-Verbose 'Starting .NET Tracer build...'
+.\DotNet-Tracer\build.ps1
+
 if (-not $skipWindows) {
 	Write-Verbose 'Starting Windows build...'
 	.\Windows\build.ps1
@@ -31,6 +35,11 @@ if (-not $skipWindows) {
 if (-not $skipMac) {
 	Write-Verbose 'Starting macOS build...'
 	.\macOS\build.ps1
+}
+
+if (-not $skipLinux) {
+	Write-Verbose 'Starting Linux build...'
+	.\Linux\build.ps1
 }
 
 Pop-Location
