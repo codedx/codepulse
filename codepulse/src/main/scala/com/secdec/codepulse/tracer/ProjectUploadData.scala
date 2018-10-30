444f6f093fef262399f765b3689d8c178d74d4bc
Lenny Halseth <Lenny.Halseth@codedx.com>
2018-10-30 12:19:43 -0400

    Fix none.get error when handling older import files

--- codepulse/src/main/scala/com/secdec/codepulse/tracer/ProjectUploadData.scala
+++ codepulse/src/main/scala/com/secdec/codepulse/tracer/ProjectUploadData.scala
@@ -87,7 +87,7 @@ object ProjectUploadData extends Loggable {
 
 			case util.Success(_) =>
 				for (target <- projectManager getProject projectId) {
-					eventBus.publish(ProcessDataAvailable(projectId.num.toString, StorageManager.getStorageFor(projectId).get, target.projectData.treeNodeData, target.projectData.sourceData, target.projectData.metadata))
+					StorageManager.getStorageFor(projectId).foreach(storage => eventBus.publish(ProcessDataAvailable(projectId.num.toString, storage, target.projectData.treeNodeData, target.projectData.sourceData, target.projectData.metadata)))
 					target.notifyLoadingFinished()
 				}
 		}
